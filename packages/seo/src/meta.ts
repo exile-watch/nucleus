84079e27257301b404c5ec5dc396e52f4a364b04
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-16 18:04:51 +0100

    chore: add titleCase util to handle url formatting

--- packages/seo/src/meta.ts
+++ packages/seo/src/meta.ts
@@ -1,4 +1,5 @@
 import {DefaultSeoProps} from "next-seo";
+import {titleCase} from "../src/utils";
 
 type Description = {
   directory: string | string[]
@@ -30,27 +31,27 @@ const metaWelcome: Meta = () => ({
 
 const metaDirectory: Meta = ({directory}) => ({
   title: `Directory`,
-  description: `Advance your ${directory} gameplay with exile.watch: Discover visual ability insights and GIFs designed to conquer new challenges.`,
+  description: `Advance your ${titleCase(directory)} gameplay with exile.watch: Discover visual ability insights and GIFs designed to conquer new challenges.`,
 })
 
 const metaEncounters: Meta = ({directory}) => ({
-  title: `${directory} Encounters`,
-  encounters: `Navigate ${directory} encounters with ease on exile.watch: Access a curated list of encounter categories for targeted visual ability insights.`
+  title: `${titleCase(directory)} Encounters`,
+  description: `Navigate ${titleCase(directory)} encounters with ease on exile.watch: Access a curated list of encounter categories for targeted visual ability insights.`
 })
 
 const metaEncountersCategories: Meta = ({directory, category}) => ({
-  title: `${category} - ${directory} Encounters`,
-  encounters: `Master the ${category} encounters in ${directory} with exile.watch: Visual ability insights and GIFs to conquer the Atlas's toughest foes.`
+  title: `${titleCase(category)} - ${titleCase(directory)} Encounters`,
+  description: `Master the ${titleCase(category)} encounters in ${titleCase(directory)} with exile.watch: Visual ability insights and GIFs to conquer the Atlas's toughest foes.`
 })
 
 const metaEncountersCategoryMaps: Meta = ({directory, category, map}) => ({
-  title: `${map} - ${category}, ${directory} Encounters`,
-  encounters: `Master ${map} encounters in ${directory} with exile.watch: Visual ability insights and GIFs for all encounters.`
+  title: `${titleCase(map)} - ${titleCase(category)}, ${titleCase(directory)} Encounters`,
+  description: `Master ${titleCase(map)} encounters in ${titleCase(directory)} with exile.watch: Visual ability insights and GIFs for all encounters.`
 })
 
 const metaEncounter: Meta = ({directory, category, encounter}) => ({
-  title: `${encounter} - ${category}, ${directory} Encounters`,
-  encounters: `Understand and conquer ${encounter} in ${directory} with exile.watch: Discover key abilities and their effects through our concise, informative GIFs.`
+  title: `${titleCase(encounter)} - ${titleCase(category)}, ${titleCase(directory)} Encounters`,
+  description: `Understand and conquer ${titleCase(encounter)} in ${titleCase(directory)} with exile.watch: Discover key abilities and their effects through our concise, informative GIFs.`
 })
 
 export {
--- packages/seo/src/utils.ts
+++ packages/seo/src/utils.ts
@@ -46,4 +46,20 @@ function generateXml() {
   return xml.trim();
 }
 
-export {generateXml}
+function titleCase(str) {
+  const lowerCaseWords = ['of'];
+  return str
+    .split('-') // Split the string into an array by dashes.
+    .map((word, index) => {
+      // Check if the word should remain in lowercase, except if it's the first word.
+      if (lowerCaseWords.includes(word) && index !== 0) {
+        return word;
+      }
+      // Capitalize the first letter of the word.
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(' ') // Join the array back into a string with spaces.
+    .replace(/(\d+)/, ' $1'); // Ensure numbers are correctly spaced.
+}
+
+export {generateXml, titleCase}
